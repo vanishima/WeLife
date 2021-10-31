5b192754771f075cc591b07d24fbb77ed2dac39a
SixTRaps <651077152@qq.com>
2021-10-31 11:45:07 -0700

    OCT31_yxj_major

--- app.js
+++ app.js
@@ -1,20 +1,67 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 let express = require("express");
 let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
+let credentials = require("./db/pseudoCredentialDB.json");
+const flash = require("express-flash");
+const session = require("express-session");
+const passport = require("passport");
+const methodOverride = require("method-override");
+
+const initializePassport = require("./public/javascripts/passport-config");
+
+initializePassport(
+  passport,
+  (username) => credentials.find((user) => user.username === username),
+  (id) => credentials.find((user) => user.id === id)
+);
+
+// initializePassport(
+//   passport,
+//   (username) => {
+//     for (let user of credentials) {
+//       if (user.username === username) {
+//         return user;
+//       }
+//     }
+//     return undefined;
+//   },
+//   (id) => {
+//     for (let user of credentials) {
+//       if (user.id === id) {
+//         return user;
+//       }
+//     }
+//     return undefined;
+//   }
+// );
 
 let indexRouter = require("./routes/index");
-// let generalRouter = require("./routes/general");
 
 let app = express();
 
+app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
-// app.use("/general", generalRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,48 @@ let express = require("express");
 let router = express.Router();
 
 // const momentDB = require("../db/momentDB.js");
-const momentDB = require("../db/pseudoMomentDB.json");
-let credentials = require("../db/pseudoCredentialDB.json");
+let momentDB = require("../db/pseudoMomentDB.json");
+// let credentials = require("../db/pseudoCredentialDB.json");
+const credentials = [];
 const fs = require("fs");
-
 const bcrypt = require("bcrypt");
-// const saltRounds = 10;
+const passport = require("passport");
+
+const initializePassport = require("../public/javascripts/passport-config");
+
+initializePassport(
+  passport,
+  (username) => credentials.find((user) => user.username === username),
+  (id) => credentials.find((user) => user.id === id)
+);
 
 /* GET home page. */
-router.get("/", function (req, res) {
+router.get("/", checkAuthenticated, function (req, res) {
   res.render("index", { title: "Express" });
+});
+
+router.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("signin.ejs");
+});
+
+router.get("/signup", checkNotAuthenticated, (req, res) => {
+  res.render("signup.ejs");
+});
+
+router.get("/home", checkAuthenticated, (req, res) => {
+  res.render("home.ejs");
+});
+
+router.get("/post", checkAuthenticated, (req, res) => {
+  res.render("post.ejs");
+});
+
+router.get("/general", checkAuthenticated, (req, res) => {
+  res.render("general.ejs");
+});
+
+router.get("/momentDB", checkAuthenticated, (req, res) => {
+  res.send(momentDB);
 });
 
 /* User auth */
@@ -23,137 +55,54 @@ function auth(req, res) {
   return true;
 }
 
-/* User Sign-In Request. */
-router.get("/userSignIn", async (req, res) => {
-  try {
-    console.log("Received sign in request");
-    res.json(credentials);
-  } catch (e) {
-    console.log("Error getting data: ", e);
-    res.status(400).send({ err: e });
-  }
-});
-// router.post("/userSignIn", async (req, res) => {
-//   try {
-//     const username = req.body.username;
-//     const password = req.body.password;
-//     // ask db to validate this user
-//     const hash = await momentDB.getPassword(username);
-//     // const hash = await credentials.
-
-//     // user does not exist
-//     if (hash == null) {
-//       res.status(401).send({ signin: "user not found" });
-//     } else {
-//       const match = await bcrypt.compare(password, hash);
-//       if (match == true) {
-//         req.session.username = username;
-//         res.sendStatus(200);
-//       } else {
-//         res.status(401).send({ signin: "wrong password" });
-//       }
-//     }
-//   } catch (e) {
-//     res.status(400).send({ err: e });
-//   }
-// });
+router.post(
+  "/signin",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 /* User Sign-Up Request. */
-router.post("/userSignUp", async (req, res) => {
-  const newUser = req.body;
-  for (let user of credentials) {
-    if (user.username === newUser.username) {
-      console.log("User exists!");
-      throw new Error("User Exists!");
-    }
+router.post("/signup", checkNotAuthenticated, async (req, res) => {
+  try {
+    console.log("Creating new user ", req);
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    credentials.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      password: hashPassword,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+    // credentials = JSON.stringify(credentials);
+
+    // fs.writeFile("./db/pseudoCredentialDB.json", credentials, (err) => {
+    //   if (err) {
+    //     res.status(400).send({ err: err });
+    //     console.log("Error registering new user");
+    //   }
+    // });
+    res.redirect("/login");
+  } catch (e) {
+    console.log("Error signing up new user: ", e);
+    // res.status(400).send({ err: e });
+    res.redirect("/signup");
   }
-  console.log("Creating new user ", newUser);
-  const userdata = {
-    username: newUser.username,
-    password: newUser.password,
-    firstname: newUser.firstname,
-    lastname: newUser.lastname,
-  };
-  credentials.push(userdata);
-  credentials = JSON.stringify(credentials);
-
-  fs.writeFile("./db/pseudoCredentialDB.json", credentials, (err) => {
-    if (err) {
-      res.status(400).send({ err: err });
-      console.log("Error registering new user");
-    }
-  });
-  res.redirect("/");
+  console.log(credentials);
 });
-// router.post("/userSignUp", async (req, res) => {
-//   console.log(req.body.username);
-//   try {
-//     const username = req.body.username;
-//     const password = req.body.password;
-//     const firstname = req.body.firstname;
-//     const lastname = req.body.lastname;
-
-//     // hash password and save to db
-//     // await bcrypt.hash(password, saltRounds, async function (err, hash) {
-//     console.log("Creating account...");
-//     const msg = await momentDB.createCredential(
-//       username,
-//       password,
-//       firstname,
-//       lastname
-//     );
-//     if (msg === "success") {
-//       // save username to session
-//       console.log("Successfully created account!");
-//       req.session.username = username;
-//       res.sendStatus(200);
-//     } else {
-//       res.status(409).send({ register: msg });
-//     }
-//     // });
-//   } catch (e) {
-//     res.status(400).send({ err: e });
-//   }
-// });
 
 /* User Log-Out Request. */
-router.get("/userLogout", async (req, res) => {
-  try {
-    if (!auth(req, res)) {
-      return;
-    }
-    delete req.session.username;
-    res.sendStatus(200);
-  } catch (e) {
-    res.status(400).send({ err: e });
-  }
-});
-
-/* Display moments in general page */
-router.get("/general", async (req, res) => {
-  try {
-    console.log("Getting data from db");
-    // const files = await momentDB.getFiles();
-    // res.send({ files: files });
-    res.json(momentDB);
-  } catch (e) {
-    console.log("Error getting data: ", e);
-    res.status(400).send({ err: e });
-  }
+router.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
 });
 
 /* Get User's homepage to show thier own posts */
 router.get("/myPosts", async (req, res) => {
   try {
-    // if (!auth(req, res)) {
-    //   return;
-    // }
-    // const username = req.session.username;
-
-    // const userPosts = await myDB.getUserPosts(username);
-
-    // res.status(200).send({ data: userPosts });
-
     console.log("Getting data from db and send it to reload.");
   } catch (e) {
     console.log("Error getting data: ", e);
@@ -205,5 +154,20 @@ router.post("/deletePost", async (req, res) => {
     res.status(400).send({ err: e });
   }
 });
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
 
 module.exports = router;
